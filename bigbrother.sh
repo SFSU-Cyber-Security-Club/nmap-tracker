7cdf4038ef135c1ac52b0a9985f79440c4576551
Kinzo <sauljcervantes2@gamil.com>
2024-01-23 19:29:28 -0800

    directory parsing not working when executing script

--- bigbrother.sh
+++ bigbrother.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
+SERVER_PORT=8080
+
+# returns xml files in the current directory
+get_xml_files() {
+    $(find . -maxdepth 1 -type f -name '*.xml')
+}
+
+# send xml files to the server
+send_xml_files() {
+    for file in $(get_xml_files); do
+        # curl -X POST -F "file=@$file" http://localhost:$SERVER_PORT/upload
+        echo "Sending $file to server..."
+    done
+}
+
+# initial send
+send_xml_files
+
 # get the number of files in the current directory
 file_count=$(ls -l | grep ^- | wc -l)
-
+# watch for changes in the current directory
 while true; do
     if [ $file_count -ne $(ls -l | grep ^- | wc -l) ]; then
-        # TODO: action when the number of files in the directory changes
-        echo "File count changed!"
+        send_xml_files
         file_count=$(ls -l | grep ^- | wc -l)
     fi
 done
