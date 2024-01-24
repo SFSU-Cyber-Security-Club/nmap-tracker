04361741aac2434e9460911d0a64d138b717f016
Kinzo <sauljcervantes2@gamil.com>
2024-01-23 23:12:03 -0800

    Added default args

--- bigbrother.sh
+++ bigbrother.sh
@@ -7,7 +7,7 @@ SERVER_PORT=8080
 DIRECTORY="."
 
 # returns xml files in the target directory
-get_xml_files() {
+function get_xml_files() {
     for file in $(ls "$DIRECTORY"); do
         if [[ ${file: -4} == ".xml" ]]; then
             echo $file
@@ -16,25 +16,41 @@ get_xml_files() {
 }
 
 # send xml files to the server
-send_xml_files() {
+function send_xml_files() {
     for file in $(get_xml_files); do
         echo "Sending $file to server..."
+
         curl -s -X POST http://$SERVER_IP:$SERVER_PORT/api/scans -H "Content-Type: application/xml" --data-binary @$DIRECTORY/$file
         echo ""
     done
 }
 
 # set up
-echo -n "Enter server IP: "
-read SERVER_IP
-echo -n "Enter server port: "
-read SERVER_PORT
-echo -n "Enter directory to watch: "
-read DIRECTORY
+echo "Setting up Big Brother..."
+# server ip address
+read -p "Enter the server IP address: " response
+if [ -z "$response" ]; then
+    echo "Using default server IP address: $SERVER_IP"
+else
+    SERVER_IP=$response
+fi
+# server port
+read -p "Enter the server port : " response
+if [ -z "$response" ]; then
+    echo "Using default server port: $SERVER_PORT"
+else
+    SERVER_PORT=$response
+fi
+# target directory
+read -p "Enter the target directory : " response
+if [ -z "$response" ]; then
+    echo "Using default server port: $DIRECTORY"
+else
+    DIRECTORY=$response
+fi
 
 # initial send
 send_xml_files
-echo ""
 echo "$STANDBY_MSG"
 
 # get the number of files in the target directory
@@ -44,7 +60,6 @@ while true; do
     if [ $file_count -ne $(ls -l $DIRECTORY | grep ^- | wc -l) ]; then
         send_xml_files
         file_count=$(ls -l $DIRECTORY | grep ^- | wc -l)
-        echo ""
         echo "$STANDBY_MSG"
     fi
 done
